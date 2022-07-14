223287a54013d493074bde712945a57de17803ff
Kyle Canamar <canamar.kyle@gmail.com>
2022-07-14 11:13:39 -0700

    creates about.html template, adds route to main_app/urls.py, defines about function in main_app/views.py, creates link on nav bar in base.html

--- main_app/urls.py
+++ main_app/urls.py
@@ -2,5 +2,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    # home route
     path('', views.home, name='home'),
+    # about route
+    path('about/', views.about, name='about'),
+    
 ]
