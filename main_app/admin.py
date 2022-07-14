55222852ee636c31bfa788c611b9646b9d4be7cd
Kyle Canamar <canamar.kyle@gmail.com>
2022-07-14 12:06:59 -0700

    creates format & set models > main_app/models.py, registers models > main_app/admin.py, runs scripts [python manage.py makemigrations] & [python manage.py migrate]

--- main_app/admin.py
+++ main_app/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Card, Format, Set
 # Register your models here.
+admin.site.register(Card)
+admin.site.register(Set)
+admin.site.register(Format)
--- main_app/models.py
+++ main_app/models.py
@@ -13,3 +13,36 @@ class Card(models.Model):
     
     def get_absolute_url(self):
         return reverse('detail', kwargs={'card_id': self.id})
+
+class Set(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("sets_detail", kwargs={"pk": self.id})
+
+FORMAT = (
+    ('MOD','Modern'),
+    ('STD','Standard'),
+    ('PNR','Pioneer'),
+    ('LGC','Legacy'),
+    ('VIN','Vintage'),
+    ('PAU','Pauper'),
+    ('CMD','Commander'),
+    ('BRW','Brawl'),
+)
+
+class Format(models.Model):
+    date = models.DateField('Date Last Played')
+    formats = models.CharField(
+        max_length=3,
+        choices=FORMAT,
+        default=FORMAT[0][0]
+    )
+
+    def __str__(self):
+        return f'{self.get_formats_display()} on {self.date}'
+    class Meta:
+        ordering = ['-date']
